ee175c8b39c6ff72541a454b2f505a8403335463
KanevDN <66564760+KanevDN@users.noreply.github.com>
2020-06-07 19:26:07 +0300

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,15 @@
+using System;
+
+using static lib1;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(lib1.stpow);
+        Console.WriteLine(lib1.rev);
+        Console.WriteLine(lib1.remove);
+        Console.WriteLine(lib1.leng);
+    }
+}
